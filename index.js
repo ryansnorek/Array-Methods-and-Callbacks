5af170701f99b76e6cb27d887c37177f014dd613
Ryan <snorekr@gmail.com>
2021-09-12 16:45:09 -0400

    Completed Task 1

--- index.js
+++ index.js
@@ -4,18 +4,28 @@ const { fifaData } = require('./fifa.js')
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Practice accessing data by console.log-ing the following pieces of data note, you may want to filter the data first 😉*/
-fifaData.filter(function(item){
-    return item.Year === 2014;
+const game = fifaData.filter(function(item){
+    return item.Year === 2014 && item.Stage === 'Final';
 })
 //(a) Home Team name for 2014 world cup final
+const homeName = game[0]['Home Team Name'];
 
 //(b) Away Team name for 2014 world cup final
+const awayName = game[0]['Away Team Name'];
 
 //(c) Home Team goals for 2014 world cup final
+const homeGoals = game[0]['Home Team Goals'];
 
 //(d) Away Team goals for 2014 world cup final
+const awayGoals = game[0]['Away Team Goals'];
 
 //(e) Winner of 2014 world cup final */
+let winner = '';
+if (homeGoals > awayGoals) winner = homeName;
+else winner = awayName;
+
+// console.log(winner)
+
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
